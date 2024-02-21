9d2610236f4d306e7ebb70a4c21ba4e8d8bf7796
whosherald <whosherald@gmail.com>
2024-02-21 09:46:52 +0200

    1st

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Leveraging Elastic Kubernetes Service for Scalable Deployments
+
+## to complete this, you should have the following
+1.Docker desktop installed on your machine \
+2.Docker hub account \
+3.AWS account \
+4.aws cli installed \
+5.Laravel  installed \
+6.eksctl installed \
+7.kubectl installed
+
+
+
+<p align="center"><a href="http://noblemulaudzi.com" target="_blank"><img src="Untitled Diagram.drawio (17).png" width="600" alt="Laravel Logo"></a></p>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to My App!</title>
+  <style>
+    body {
+      background-color: #f2f2f2;
+      font-family: Arial, sans-serif;
+    }
+    .container {
+      text-align: center;
+      margin-top: 200px;
+    }
+    h1 {
+      color: #333;
+      font-size: 36px;
+      margin-bottom: 20px;
+    }
+    p {
+      color: #666;
+      font-size: 24px;
+      margin-bottom: 30px;
+    }
+    .success-message {
+      color: #33cc33;
+      font-size: 24px;
+      font-weight: bold;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Hi Noble great job</h1>
+    <p>Your application has been successfully deployed to Amazon EKS.</p>
+    <p class="success-message">Congratulations!</p>
+  </div>
+</body>
+</html>
